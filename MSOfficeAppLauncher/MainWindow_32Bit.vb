f27ce54a3d556c1d9a3dfff5d03acb0e3db1c9d9
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-05-21 22:05:53 -0400

    The Version History buttons on the About tab...
    now go to my GitHub release feeds.

--- MSOfficeAppLauncher/MainWindow_32Bit.vb
+++ MSOfficeAppLauncher/MainWindow_32Bit.vb
@@ -119,7 +119,7 @@ Public Class Main
     'Open the version history - 32-bit
     Private Sub ThreeTwoBitHist_Click(sender As System.Object, e As System.EventArgs) Handles ThreeTwoBitHist.Click
         If My.Settings.changelogDisplayMode = True Then
-            Process.Start("http://drews-apps.weebly.com/3/feed")
+            Process.Start("https://github.com/DrewNaylor/MSOfficeAppLauncherNext32/releases.atom")
         Else
             HTML32BitVerHist.ShowDialog()
         End If
@@ -129,7 +129,7 @@ Public Class Main
     'Open the version history - 64-bit
     Private Sub SixFourVerHistButton_Click(sender As System.Object, e As System.EventArgs) Handles SixFourVerHistButton.Click
         If My.Settings.changelogDisplayMode = True Then
-            Process.Start("http://drews-apps.weebly.com/2/feed")
+            Process.Start("https://github.com/DrewNaylor/MSOfficeAppLauncherNext64/releases.atom")
         Else
             HTML64BitVerHist.ShowDialog()
         End If
