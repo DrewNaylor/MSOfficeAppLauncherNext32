b657a0ef239a912e46a4eb29497be6a462308629
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-05-21 21:37:18 -0400

    Updated size and text of Notes groupbox stuff.

--- MSOfficeAppLauncher/MainWindow_32Bit.Designer.vb
+++ MSOfficeAppLauncher/MainWindow_32Bit.Designer.vb
@@ -651,7 +651,7 @@ Partial Class Main
         Me.GroupBox3.Controls.Add(Me.TextBox2)
         Me.GroupBox3.Location = New System.Drawing.Point(9, 271)
         Me.GroupBox3.Name = "GroupBox3"
-        Me.GroupBox3.Size = New System.Drawing.Size(471, 141)
+        Me.GroupBox3.Size = New System.Drawing.Size(471, 156)
         Me.GroupBox3.TabIndex = 2
         Me.GroupBox3.TabStop = False
         Me.GroupBox3.Text = "Notes"
@@ -664,7 +664,7 @@ Partial Class Main
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ReadOnly = True
         Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
-        Me.TextBox2.Size = New System.Drawing.Size(458, 115)
+        Me.TextBox2.Size = New System.Drawing.Size(458, 130)
         Me.TextBox2.TabIndex = 0
         Me.TextBox2.Text = resources.GetString("TextBox2.Text")
         '
@@ -672,9 +672,9 @@ Partial Class Main
         '
         Me.GroupBox2.Controls.Add(Me.LinkLabel2)
         Me.GroupBox2.Controls.Add(Me.LinkLabel1)
-        Me.GroupBox2.Location = New System.Drawing.Point(9, 418)
+        Me.GroupBox2.Location = New System.Drawing.Point(9, 433)
         Me.GroupBox2.Name = "GroupBox2"
-        Me.GroupBox2.Size = New System.Drawing.Size(471, 76)
+        Me.GroupBox2.Size = New System.Drawing.Size(471, 61)
         Me.GroupBox2.TabIndex = 1
         Me.GroupBox2.TabStop = False
         Me.GroupBox2.Text = "My Web Sites"
