3b2ff22e8879cd9e534f58d95a5bffa3722dfb4a
Drew Naylor <DrewNaylor@users.noreply.github.com>
2016-08-02 13:23:38 -0400

    Improving the build date thing a little.

--- MSOfficeAppLauncher/MainWindow_32Bit.vb
+++ MSOfficeAppLauncher/MainWindow_32Bit.vb
@@ -53,7 +53,7 @@ Public Class Main
         TextBox1.AppendText("It can also clear up space on your desktop." & Environment.NewLine)
         TextBox1.AppendText("I am not intending on infringing on Microsoft's copyrights, so this is only a shortcut application." & Environment.NewLine)
         TextBox1.AppendText("" & Environment.NewLine)
-        TextBox1.AppendText("App compiled on " & InsertBuildDate & Environment.NewLine)
+        TextBox1.AppendText("App compiled on " & InsertBuildDate & "(This date is local to your time.)" & Environment.NewLine)
         TextBox1.AppendText("" & Environment.NewLine)
         TextBox1.AppendText("Copyright notice: Office, Microsoft Office, Word, Excel, PowerPoint, and all related words (by MSFT) are Copyright [a long time ago]-[sometime in the future] Microsoft Corp. All Rights Reserved for Microsoft's copyrights." & Environment.NewLine)
         TextBox1.AppendText("" & Environment.NewLine)
