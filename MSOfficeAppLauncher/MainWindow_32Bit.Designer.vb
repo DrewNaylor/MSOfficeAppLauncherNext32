9b4f9712a64295f3b65743455eedc97d8e575a04
Drew Naylor <DrewNaylor@users.noreply.github.com>
2020-10-06 22:19:31 -0400

    Show vertical scrollbars for Notes box.

--- MSOfficeAppLauncher/MainWindow_32Bit.Designer.vb
+++ MSOfficeAppLauncher/MainWindow_32Bit.Designer.vb
@@ -658,6 +658,7 @@ Partial Class Main
         Me.TextBox2.Multiline = True
         Me.TextBox2.Name = "TextBox2"
         Me.TextBox2.ReadOnly = True
+        Me.TextBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
         Me.TextBox2.Size = New System.Drawing.Size(458, 133)
         Me.TextBox2.TabIndex = 0
         '
